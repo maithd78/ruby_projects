a077b449af9e09a2cd144a05aa6169a0f7e8b3f3
Maithd O11 <90709583+maithd78@users.noreply.github.com>
2025-03-21 15:00:09 +0900

    rename files and folders to follow pattern

--- caesar_cipher/caesar_cipher.rb
+++ caesar_cipher/caesar_cipher.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 def caesar(string, shift)
-  result = Array.new
+  result = []
   downcase_alphabet = ('a'..'z').to_a
   upcase_alphabet = ('A'..'Z').to_a
   alphabet = Array(downcase_alphabet + upcase_alphabet)
-  split = string.split("")
-  split_hash = Hash[(0...split.size).zip split]
-  whitespace = split_hash.select {|k,v| !alphabet.include?(v)}
-  split.delete_if {|e| !alphabet.include?(e)}
+  split = string.chars
+  split_hash = ((0...split.size).zip split).to_h
+  whitespace = split_hash.reject { |_k, v| alphabet.include?(v) }
+  split.delete_if { |e| !alphabet.include?(e) }
   split.each do |letter|
     if letter == letter.upcase
       swap_index = upcase_alphabet.index(letter) + shift
-      swap_index -= 26 if swap_index > 25 
+      swap_index -= 26 if swap_index > 25
       result.push(upcase_alphabet[swap_index])
     else
       swap_index = downcase_alphabet.index(letter) + shift
@@ -19,19 +21,19 @@ def caesar(string, shift)
     end
   end
   whitespace.each_pair do |index, value|
-    result.insert(index,value)
+    result.insert(index, value)
   end
-puts result.join
+  puts result.join
 end
 
-puts "enter plaintext to cipher"
+puts 'enter plaintext to cipher'
 str = gets.chomp
 
 shift = 0
-puts "enter shift"
+puts 'enter shift'
 until shift != 0
   shift = gets.chomp.to_i
-  puts "not a number" if shift == 0
+  puts 'not a number' if shift.zero?
 end
 
-caesar(str,shift)
+caesar(str, shift)
