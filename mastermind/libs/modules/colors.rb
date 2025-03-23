b0708d8f1741dfc44e95b61f642968ef3dc75ed5
Maithd O11 <90709583+maithd78@users.noreply.github.com>
2025-03-23 22:49:56 +0900

    initial commit

---
+++ mastermind/libs/modules/colors.rb
@@ -0,0 +1,6 @@
+# Module to store colors
+module Colors
+  def list_of_colors
+    %w[red green blue yellow orange purple]
+  end
+end
---
+++ mastermind/main.rb
@@ -0,0 +1,16 @@
+# Create Game method to start game, initialize an array to contain the colors for the game for player and computer
+# init the array of available color and guesses
+# create new method to get whether the player chooses the colors or if the computer chooses the colours
+# skip 2 for now
+# let computer choose colours from an array of colors do this in the computer class
+# store this info in the computer class
+# create round loop in game.rb
+# until round == 0 do ?
+# let the player guess 4 colors one after the other and then confirm the choices at the end
+# do this in player
+# at the end of that loop send the info to the array containing colors for player in game and check if any matches are
+# found in computer.
+# then if there are matches print if colors right or locations right.
+require_relative 'libs/game'
+require_relative 'libs/modules/colors'
+require_relative 'libs/computer'
