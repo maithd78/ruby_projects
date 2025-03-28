3085043e7316b73b3b6c2b1967297549bd0176e7
Maithd O11 <90709583+maithd78@users.noreply.github.com>
2025-03-28 15:27:00 +0900

    add ability to create a code by the player

--- mastermind/libs/player.rb
+++ mastermind/libs/player.rb
@@ -3,14 +3,27 @@ require_relative 'modules/colors'
 class Player
   include Colors
 
-  def create_code_for_computer
-    puts "available colors #{list_of_colors.join(',')}"
-    @player_color_code = []
-    until @player_color_code.size == 4
-      puts "what is the color in peg #{@player_color_code.size + 1}"
-      @color = gets.chomp.downcase
-      @player_color_code.push(@color) if list_of_colors.any?(@color)
-      puts 'invalid color' if list_of_colors.none?(@color)
+  attr_reader :player_code_digits
+
+  def player_create_color_code
+    @player_code = []
+    puts "available colors:\n #{list_of_colors.join(', ')}"
+    until @player_code.size == 4
+      puts "what is the color in peg #{@player_code.size + 1}"
+      puts 'invalid color, Try again' until list_of_colors.include?(@current_color = gets.chomp.downcase)
+      @player_code.push(@current_color)
     end
+    convert_to_digits
+  end
+
+  def convert_to_digits
+    @colors = list_of_colors.to_h { |item| [item, list_of_colors.index(item) + 1] }
+    @player_code_digits = []
+    if @player_code.empty?
+      @color_code.each { |e| @player_code_digits.push(@colors.values_at(e)) }
+    else
+      @player_code.each { |e| @player_code_digits.push(@colors.values_at(e)) }
+    end
+    @player_code_digits = @player_code_digits.join.to_i
   end
 end
