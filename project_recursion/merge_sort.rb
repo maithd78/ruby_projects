f701393ca702b41359506269e13035852060cfd0
Maithd O11 <90709583+maithd78@users.noreply.github.com>
2025-04-19 12:19:48 +0900

    somehow

--- project_recursion/merge_sort.rb
+++ project_recursion/merge_sort.rb
@@ -1,15 +1,28 @@
-def merge_sort(array, sorted_array = [])
-  n = array.length
-  if array.length <= 2
-    sorted_array << array
-    return
+def merge_sort(array)
+  if array.length <= 1
+   return array
   else
-    merge_sort(array.values_at(1..n))
-    merge_sort(array.values_at(n+1..array.length+1))
+    mid = array.length / 2
+    left = merge_sort(array.slice(0,mid))
+    right = merge_sort(array.slice(mid,array.length))
+    merge(left,right)
   end
-  p sorted_array
 end
 
-arr = [5,3,2,6,1,4]
+def merge(left,right)
+  sorted = []
+  until left.empty? || right.empty?
+    if left.first < right.first
+      sorted << left.shift
+    else
+      sorted << right.shift
+    end
+  end
+  p sorted + left + right
+end
+
+# arr = Array.new(8) { rand(1..9) }
+
+arr = [2,4,6,5,1,3,8,7]
 
 merge_sort(arr)
